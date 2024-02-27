1284ceda8abb7f0b67c844459926b655974d8be7
AliAhmad05 <aliaurangzaib03@gmail.com>
2024-02-27 19:02:35 +0500

    First Commit

--- e2e/Fixtures/apiUserData.js
+++ e2e/Fixtures/apiUserData.js
@@ -2,7 +2,7 @@ const randomNum1 = Math.floor(Math.random() * (100 - 1) + 1);
 const randomNum2 = Math.floor(Math.random() * (100 - 1) + 1);
 
 const requestData = {
-  userName: `Hassan-Zaheer${randomNum1}${randomNum2}`,
+  userName: `AliAhmad${randomNum1}${randomNum2}`,
   password: 'testing@Playwright05',
 };
 
