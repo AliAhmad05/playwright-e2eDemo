448cf5c3b2601a2e1820f6dd982c269a3aa52aa8
AliAhmad05 <aliaurangzaib03@gmail.com>
2024-02-27 22:04:40 +0500

    fixed retry code

--- playwright.config.js
+++ playwright.config.js
@@ -12,17 +12,16 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   /* Run tests in files in parallel */
-  timeout: 5 * 60 * 1000,
+  timeout: 2 * 60 * 1000,
+
   
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-
-  retries:3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
